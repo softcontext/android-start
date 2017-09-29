d44475cab1609ba343a47e6e348baad207ebcef8
Seokwon SONG <softcontext@gmail.com>
2017-09-29 15:55:26 +0900

    13. Track User Flows

--- app/src/main/java/com/google/firebase/codelab/friendlychat/MainActivity.java
+++ app/src/main/java/com/google/firebase/codelab/friendlychat/MainActivity.java
@@ -51,6 +51,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.analytics.FirebaseAnalytics;
 import com.google.firebase.appindexing.Action;
 import com.google.firebase.appindexing.FirebaseAppIndex;
 import com.google.firebase.appindexing.FirebaseUserActions;
@@ -125,6 +126,8 @@ public class MainActivity extends AppCompatActivity
     // XXX
     // Firebase instance variables
     private FirebaseRemoteConfig mFirebaseRemoteConfig;
+    // XXX: Step 13
+    private FirebaseAnalytics mFirebaseAnalytics;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -346,6 +349,9 @@ public class MainActivity extends AppCompatActivity
 
         // Fetch remote config.
         fetchConfig();
+
+        // XXX: Step 13
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
     }
 
     @Override
@@ -448,12 +454,23 @@ public class MainActivity extends AppCompatActivity
                 }
             }
         } else if (requestCode == REQUEST_INVITE) { // XXX: Step 12
+            // XXX: Step 13
             if (resultCode == RESULT_OK) {
+                Bundle payload = new Bundle();
+                payload.putString(FirebaseAnalytics.Param.VALUE, "sent");
+                mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SHARE,
+                        payload);
                 // Check how many invitations were sent and log.
-                String[] ids = AppInviteInvitation.getInvitationIds(resultCode, data);
+                String[] ids = AppInviteInvitation.getInvitationIds(resultCode,
+                        data);
                 Log.d(TAG, "Invitations sent: " + ids.length);
             } else {
-                // Sending failed or it was canceled, show failure message to the user
+                Bundle payload = new Bundle();
+                payload.putString(FirebaseAnalytics.Param.VALUE, "not sent");
+                mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SHARE,
+                        payload);
+                // Sending failed or it was canceled, show failure message to
+                // the user
                 Log.d(TAG, "Failed to send invitation.");
             }
         }
